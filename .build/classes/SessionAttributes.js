faa078bccbe89ca018b3972fa6add9b5e5f37d9e
SamuliRukkila <samuli.rukkila@hotmail.fi>
2019-10-03 20:48:10 +0300

    removed autocreated .build-folder; added Event.ts -class

--- .build/classes/SessionAttributes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var SessionAttributes = /** @class */ (function () {
-    function SessionAttributes() {
-    }
-    return SessionAttributes;
-}());
-exports.SessionAttributes = SessionAttributes;
-//# sourceMappingURL=SessionAttributes.js.map
