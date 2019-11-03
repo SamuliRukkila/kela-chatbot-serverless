b3240da0ec18b10d1895f44980ad325b7c6d1469
SamuliRukkila <samuli.rukkila@hotmail.fi>
2019-11-03 21:30:09 +0200

    Updated gitignore

--- test.js
+++
@@ -1,9 +0,0 @@
-const moment = require('moment-timezone');
-const AWS = require('aws-sdk');
-AWS.config.update({ region: 'eu-west-1' });
-const dynamo = new AWS.DynamoDB();
-
-const time ='2019-08-09T15:00:00';
-
-console.log(moment(time).tz('Europe/Helsinki').format('YYYY-MM-DDTHH:mm:ss'));
-
